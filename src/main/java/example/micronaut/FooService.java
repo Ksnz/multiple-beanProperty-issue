303a730beeb0df7bcf291e8f37bf84063300612b
Aleksei Ksenzhik <aleksei.ksenzhik@vizor-games.com>
2022-05-13 15:28:08 +0300

    import fix

--- src/main/java/example/micronaut/FooService.java
+++ src/main/java/example/micronaut/FooService.java
@@ -1,6 +1,5 @@
 package example.micronaut;
 
-import example.micronaut.config.BarProperties;
 import example.micronaut.strategy.BarStrategy;
 import io.micronaut.context.annotation.Context;
 import jakarta.inject.Inject;
